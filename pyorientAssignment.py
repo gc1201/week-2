b2bc9ad4f0de0c2917392d10e5b557843b3b6220
gc1201 <caoguangyue@dyn-160-39-195-158.dyn.columbia.edu>
2015-09-23 19:19:00 -0400

    Assignment 2_OrientDB
    Analysis a set of data accessing OrientDB through Python

--- pyorientAssignment.py
+++ pyorientAssignment.py
@@ -1,9 +1,8 @@
-
 import pyorient
 import sys
 
 client = pyorient.OrientDB("localhost", 2424)
-session_id = client.connect("root", "admin")
+session_id = client.connect("root", "474F1CBE549F9E33FC8A0793C7819485072DAE07A4B704138010F28A28B69DF9")
 db_name = "soufun"
 db_username = "admin"
 db_password = "admin"
@@ -15,11 +14,11 @@ else:
 	print "database [" + db_name + "] does not exist! session ending..."
 	sys.exit()
 
-lat1 = 22.532498
-lat2 = 22.552317
+lat1 = 22.536870
+lat2 = 22.553872
 
-lng1 = 114.044329
-lng2 = 114.076644
+lng1 = 114.026551
+lng2 = 114.060868
 
 query = 'SELECT FROM Listing WHERE latitude BETWEEN {} AND {} AND longitude BETWEEN {} AND {}'
 
@@ -40,10 +39,26 @@ for record in records:
 	print record.price
 
 
+s = 0
+for record in records:
+    s += record.price
+    
+    
+miNum = 10000000   
+for record in records:
+    if record.price < miNum:
+        miNum = record.price
+
+maNum = 50
+for record in records:
+    if record.price > maNum:
+        maNum = record.price      
+    
+
 # [PRINT OUT THE RESULTING VALUES BY CONCATENATING THEM TO THESE LINES TO CHECK YOUR WORK]
 
-print 'min price: '
-print 'max price: ' 
-print 'average price: '
+print 'min price: '+ str(miNum)
+print 'max price: '+ str(maNum) 
+print 'average price: '+ str(s/numListings)
 
 client.db_close()
